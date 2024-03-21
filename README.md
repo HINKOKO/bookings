eca98e743a56d526e59bdaf6a82cf30ada3338af
HINKOKO <piotr62@live.fr>
2024-03-21 15:00:21 +0100

    Updating go logic and serving static files

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # **GO - Bookings project**
 
+A pretty cool booking and reservation project.
 
-
+- Built in Go 1.18
+- Uses the [chi router](https://github.com/go-chi/chi)
+- Uses [SCS Session management](https://github.com/alexedwards/scs) package
+- Uses [nosurf](https://github.com/justinas/nosurf) package
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,10 +10,6 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
@@ -21,6 +17,10 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// File server
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
